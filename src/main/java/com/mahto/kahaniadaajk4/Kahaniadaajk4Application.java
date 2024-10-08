f90b54e96b6b1a95bc930035f539d52f26ed67f9
Raj-mahto <raj.mahto@rthetapi.com>
2024-10-08 16:27:17 +0530

    after setup

---
+++ src/main/java/com/mahto/kahaniadaajk4/Kahaniadaajk4Application.java
@@ -0,0 +1,14 @@
+package com.mahto.kahaniadaajk4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kahaniadaajk4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kahaniadaajk4Application.class, args);
+		System.out.println("start.............");
+	}
+
+}
---
+++ src/test/java/com/mahto/kahaniadaajk4/Kahaniadaajk4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahto.kahaniadaajk4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kahaniadaajk4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
